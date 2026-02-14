657876de3ef62f635b5136067b35af11439b332f
Mohamed Reda Mahfouz <mohamed.reda741@gmail.com>
2026-02-14 22:45:56 +0200

    [ADD] api/time_off: implement time off management endpoints
    - Added GET endpoint to fetch employee time off requests with pagination and filters
    - Added GET endpoint to retrieve leave types with allocations for the employee
    - Added POST endpoint to create time off requests with validation for required fields
    - Implemented timezone conversion for date fields in responses

---
+++ account_updates/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    Copyright (C) 2023.
+#    Author: Eng.Mohamed Reda Mahfouz (<mohamed.reda741@gmail.com>)
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+##################################################################################
+
+from . import models
---
+++ account_updates/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    Copyright (C) 2023.
+#    Author: Eng.Mohamed Reda Mahfouz (<mohamed.reda741@gmail.com>)
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+##############################################################################
+
+{
+    'name': "Account Updates",
+
+    'summary': """
+            This module is for customization purpose developed By Mohamed Reda Mahfouz To modify the standard apps of ...
+
+        """,
+
+    'description': """
+        This module is for customization purpose developed By Mohamed Reda Mahfouz
+    """,
+
+    'author': "Mohamed Reda Mahfouz",
+    'contributors': "Mohamed Reda Mahfouz <mohamed.reda741@gmail.com>",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','account'],
+
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [
+    ],
+    'license': 'OEEL-1',
+
+}
