3fcc98d19119a78b9440218257450bd5a8834618
Mohamed Reda Mahfouz <mohamed.reda741@gmail.com>
2026-07-04 19:30:01 +0300

    [IMP] real_estate_management: Update project_id field definition - Modified project_id field to remove readonly attribute

--- real_estate_management/models/sale_order.py
+++ real_estate_management/models/sale_order.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    project_id = fields.Many2one('property.project', string='Project', readonly=True)
+    project_id = fields.Many2one('property.project', string='Project',)
     property_sale_id = fields.Many2one('property.sale', string="Property Sale")
